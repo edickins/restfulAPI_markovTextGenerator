568ed8c26c7c913da8ca825e3e846be82c09d0ec
ecdickins <ed.dickins@bleepbloop.net>
2022-08-09 15:16:53 +0100

    added a single route for markovtext (change of name from original project which was text)

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { request } = require('express');
+
+// routes
+const markovtext = require('./routes/markovtext');
 
 // Load in env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// mount routes
+app.use('/api/v1/markovtext/', markovtext);
 
 const PORT = process.env.PORT || 3000;
 
