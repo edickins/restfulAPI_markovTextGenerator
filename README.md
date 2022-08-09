7a56befeeb8f20aa23412b89fea272aab3af0e04
ecdickins <ed.dickins@bleepbloop.net>
2022-08-10 00:46:45 +0100

    added reminder on how to make the server.js app run continually on the server using PM2 and added Access-Control-Allow-Origin CORS policy as middleware to server.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # restfulAPI_markovTextGenerator
 generate text using markov chain ngl generator exposing content on restful endpoints
+
+ pm2 start app.js
--- server.js
+++ server.js
@@ -18,6 +18,16 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
+
 // mount routes
 app.use('/api/v1/markovtext/', markovtext);
 app.use('/api/v1/ascii/', ascii);
