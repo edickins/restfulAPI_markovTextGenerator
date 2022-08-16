285f93cc8fb5d842cbf046ba14ccdcd3d210ef31
ecdickins <ed.dickins@bleepbloop.net>
2022-08-16 09:57:30 +0100

    added test page to experiment with cleaning data loaded through .txt files using regex

---
+++ modules/cleanText.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readFile('../data/text/soft/diy.txt', 'utf8', (err, data) => {
+	const pattern = /\w*\s?[\.|?]/gi;
+
+	match = data.match(pattern);
+	if (match) {
+		console.log(match);
+	}
+});
