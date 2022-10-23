9fa3335926dccd7a8c97710bf6bb53742d2fb413
ecdickins <ed.dickins@bleepbloop.net>
2022-10-23 17:01:43 +0100

    added mongoose middleware. markovtext.js controller connects to mongodb markovText db. markovText RESTful endpoint now supports POST method. Formdata controlled with Formidable package. POST endpoint handles single or multiple file uploads. https://winter-meteor-201299.postman.co/workspace/itunes-library-api~277c6e1e-0d9a-4c51-a847-51823cd91573/request/8760520-ec5aec65-b9a3-43c1-af5e-d15607c7b3a7 endpoint for testing

--- controllers/markovtext.js
+++ controllers/markovtext.js
@@ -1,6 +1,8 @@
 const generator = require('../modules/markovTextGenerator.js');
 const fsExtra = require('fs-extra');
 const formidable = require('formidable');
+const connectDB = require('../config/db');
+const processFileUploads = require('../modules/processFileUploads');
 
 // @desc    return text generated from the default markov text sources
 // @route   GET /api/v1/markovtext
@@ -17,46 +19,56 @@ exports.getMarkovText = async (req, res, next) => {
 };
 
 exports.addMarkovText = async (req, res, next) => {
+	//connect to database
+	const conn = await connectDB();
+
 	fsExtra.emptyDir('./uploads', err => {
 		if (err) {
 			res.status(500).json({ success: false, err: err });
 			return;
 		}
 
-		const form = formidable({
-			uploadDir: './uploads',
-			multiples: true,
-			keepExtensions: true,
-		});
-
-		//@err - Error object
-		//@fields - Object - Any fields uploaded in the formData
-		//@files - Object - Any files uploaded with the formData
-		form.parse(req, (err, fields, files) => {
-			if (err !== null) {
-				res.status(500).json({ success: false, err: err });
-			}
-
-			const fileObjs = files.file.map((file, i) => {
-				return {
-					name: fields.name[i],
-					description: fields.description[i],
-					tags: fields.fileTags[i],
-					newFilename: file.newFilename,
-					originalFilename: file.originalFilename,
-				};
+		try {
+			const form = formidable({
+				uploadDir: './uploads',
+				multiples: true,
+				keepExtensions: true,
 			});
 
-			console.log(fileObjs);
+			//@err - Error object
+			//@fields - Object - Any fields uploaded in the formData
+			//@files - Object - Any files uploaded with the formData
+			form.parse(req, (err, fields, files) => {
+				if (err !== null) {
+					res.status(500).json({ success: false, err: err });
+				}
 
-			res.json({
-				success: true,
-				msg: `files uploaded`,
-				files: files,
-				fields: fields,
+				// convert files.file into an Array if it is a single Object
+				files.file = [].concat(files.file);
+
+				const fileObjs = files.file.map((file, i) => {
+					return {
+						name: fields.name[i],
+						description: fields.description[i],
+						tags: fields.fileTags[i],
+						newFilename: file.newFilename,
+						originalFilename: file.originalFilename,
+					};
+				});
+
+				console.log(fileObjs);
+
+				const documents = processFileUploads(fileObjs);
+
+				res.status(201).json({
+					success: true,
+					msg: `files uploaded`,
+					files: files,
+					fields: fields,
+				});
 			});
-		});
+		} catch (err) {
+			res.status(500).json({ sucess: false, error: err });
+		}
 	});
-
-	// processFileUploads();
 };
--- modules/processFileUploads.js
+++ modules/processFileUploads.js
@@ -1,3 +1,21 @@
 const fs = require('fs');
 
-module.exports = processFileUploads = () => {};
+const processData = data => {
+	const regex = /([^!\.?]*[\!\.\?])/g;
+	return data.match(regex);
+};
+
+module.exports = processFileUploads = fileObjs => {
+	const processedTexts = [];
+
+	/* file.fileObjs.map(fileObj =>{
+
+  }) */
+
+	const data = fs.readFileSync(`./uploads/${fileObjs[0].newFilename}`, 'utf-8');
+
+	const processedData = processData(data);
+	console.log(processedData[0]);
+
+	return processedTexts;
+};
